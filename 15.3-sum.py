7593e42102c4886043c63644422d8ec725e83379
Jubin Jacob Varughese <vjubin@hotmail.com>
2026-05-02 19:56:16 -0400

    Fixed minor errors

---
+++ 15.3-sum.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=15 lang=python3
+#
+# [15] 3Sum
+#
+
+# @lc code=start
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        
+# @lc code=end
+
